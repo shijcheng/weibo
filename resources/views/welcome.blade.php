d69157ba0edbd4f59e0c1863d4671172d84336b2
shijiacheng <sjccc9@163.com>
2020-07-03 09:57:29 +0000

    update welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    hello shijiacheng
                 </div>
 
                 <div class="links">
