6fa6ad21300d1623f488ab79ae7c53f481c6e184
shijiacheng <sjccc9@163.com>
2020-07-06 03:24:38 +0000

    基础页面

--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -1,9 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Weibo App</title>
-</head>
-<body>
+@extends('layouts.default')
+@section('title', '关于')
+@section('content')
     <h1>帮助页</h1>
-</body>
-</html>
+@stop
