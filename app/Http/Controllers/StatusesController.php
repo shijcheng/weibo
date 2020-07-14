d3ebb85cf71d25ec49f5a1bb67a5aa1efd5817b0
shijiacheng <sjccc9@163.com>
2020-07-14 10:26:43 +0000

    首页微博列表

---
+++ app/Http/Controllers/StatusesController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class StatusesController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * 发布微博
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
+     * @author shijiacheng
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'content' => 'required|max:140'
+        ]);
+
+        Auth::user()->statuses()->create([
+            'content' => $request['content']
+        ]);
+        session()->flash('success', '发布成功！');
+        return redirect()->back();
+    }
+
+}
