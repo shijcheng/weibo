5cff2138d2b129822c8a6307fb9da1c8c9db88f0
shijiacheng <sjccc9@163.com>
2020-07-15 08:42:49 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -70,4 +70,50 @@ class User extends Authenticatable
             ->orderBy('created_at', 'desc');
     }
 
+    // 粉丝列表
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    // 关注列表
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    /**
+     * 关注用户
+     *
+     * @param $user_ids
+     * @author shijiacheng
+     */
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    /**
+     * 取关用户
+     *
+     * @param $user_ids
+     * @author shijiacheng
+     */
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
+
 }
+
